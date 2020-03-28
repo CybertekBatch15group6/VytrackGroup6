8d7d1286597cc871284327594d7fbe4dbd6cb697
raufgasan <raufgasan@gmail.com>
2020-03-28 15:11:23 -0700

    BO6-66

---
+++ src/test/java/com/automation/tests/vyteck/sprint_01/BO6_66.java
@@ -0,0 +1,16 @@
+package com.automation.tests.vyteck.sprint_01;
+
+public class BO6_66 {
+    /*
+
+    As a store manager and Sales manager I should be able to create Vehicle
+
+1.Verify that Store manager or sales manager should be able to see all vehicle information once
+navigate to a Vehicle page
+2. Verify that Store manager or sales manager should be able to create and cancel car
+3.Verify that Store manager or sales manager should be able to add Vehicle Module and Vehicle Make
+4.Verify that Store manager or sales manager should be able to edit or delete the car
+5.Verify that Store manager or sales manager should be able to add an event
+6. Verify that Store manager or sales manager should be able to reset Grid by click on Grid setting
+     */
+}
