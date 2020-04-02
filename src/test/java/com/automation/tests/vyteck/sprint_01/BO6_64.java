a919cac2d25cac792d2803b8e3ef273b936ee89b
cn6720 <cns6720@gmail.com>
2020-04-02 15:59:23 -0700

    Update

--- src/test/java/com/automation/tests/vyteck/sprint_01/BO6_64.java
+++ src/test/java/com/automation/tests/vyteck/sprint_01/BO6_64.java
@@ -1,10 +1,10 @@
 
 package com.automation.tests.vyteck.sprint_01;
-
 import com.automation.pages.*;
 import com.automation.pages.t2_Fleet.*;
-import com.automation.tests.vyteck.AbstractTestBase;
+import com.automation.tests.vyteck.*;
 import org.testng.annotations.*;
+import java.util.*;
 import static org.testng.Assert.*;
 
 public class BO6_64 extends AbstractTestBase {
@@ -18,6 +18,7 @@ public class BO6_64 extends AbstractTestBase {
     (Add Attachment, Add Note, Add Event)
     5.Verify That Store Manager Or Sales Manager Should Be Able To Reset The Grid
     By Click On Grit Setting.*/
+
     @Test(description = "User Story: 64")
     public void verifyAccessToVehicleCostsPage() {
 
@@ -28,8 +29,23 @@ public class BO6_64 extends AbstractTestBase {
         page.navigateTo("Fleet", "Vehicle Costs");
 
         String actual = page.getVehicleCostsPageText();
-        String expected = "All Vehicle Costs";
+        String expected = "Vehicle Costs";
 
         assertEquals(actual, expected);
+    }
+
+    @Test(description = "User Story: 64")
+    public void verifyVehicleCostsCreation() {
+
+        LoginPage login = new LoginPage();
+        VehicleCostsPage page = new VehicleCostsPage();
+
+        login.login();
+        page.navigateTo("Fleet", "Vehicle Costs");
+        page.clickToCreate();
+        page.submit();
+
+        List<String> expected = Arrays.asList("Road Assistance", "$1,000.00", "Apr 2, 2020", "Flat Tire");
+        assertEquals(page.actual(), expected);
     }
 }
